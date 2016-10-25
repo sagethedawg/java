1860899ab8cfee7f6c3a6854bb6137ee449da477
sagethedawg <sagethedawg@gmail.com>
2016-10-25 13:04:51 -0700

    Add files via upload

---
+++ code1/Array1.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+class Array1 {
+public static void main (String a[]){
+int i;
+int[] ia;
+ia = new int[21];
+ia[0] = -2;
+ia[1] = 0;
+ia[2] = -1;
+ia[3] = 1;
+ia[4] = 0;
+ia[5] = 1;
+ia[6] = -1;
+ia[7] = 1;
+ia[8] = -1;
+ia[9] = 0;
+
+System.out.println("Array unsorted\n");
+for (i = 0; i <= 11; i++){
+System.out.print(ia[i]+" ");
+}
+System.out.println();
+}
+}
---
+++ code1/Cwrite.java
@@ -0,0 +1,16 @@
+class Cwrite {
+	public static void main (String a[]){
+		int i,j,n = 0;
+		String c;
+		for (j = 0; j < 2000000; j++){
+			for(i = 0; i < 700; i++){
+				c = " dat hurts";
+				
+				System.out.print(c);
+				n++;
+				
+			}
+			System.out.println();
+			}
+			}
+			}
